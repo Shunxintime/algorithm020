ef9c94384ba143f186066f9ac025f683d731cdc7
shunxintime <ivy.csx@gmail.com>
2020-11-21 19:34:01 +0800

    add moving zeros

---
+++ Week1/lc283.py
@@ -0,0 +1,24 @@
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         j = 0
+#         for i in range(len(nums)):
+#             if nums[i] != 0:
+#                 nums[j] = nums[i]
+#                 j+=1
+#         for i in range(j,len(nums)):
+#             nums[i] = 0
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i],nums[j] = nums[j],nums[i]
+                j+=1
+
